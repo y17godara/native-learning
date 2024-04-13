9fa7188adbea4ace13c0c4c639b37a1d96e0b9f6
Yash Godara <yash17godara@gmail.com>
2024-04-14 01:50:58 +0530

    index page

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>index.jsx</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
